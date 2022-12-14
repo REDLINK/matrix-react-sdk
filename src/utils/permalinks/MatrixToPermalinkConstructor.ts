a00d88571507b5c942626df14212ec3289fdb5b7
Maurice Paul <paul@redlink.de>
2022-12-14 12:13:30 +0100

    adjust invite-link

--- src/utils/permalinks/MatrixToPermalinkConstructor.ts
+++ src/utils/permalinks/MatrixToPermalinkConstructor.ts
@@ -16,7 +16,7 @@ limitations under the License.
 
 import PermalinkConstructor, { PermalinkParts } from "./PermalinkConstructor";
 
-export const host = "matrix.to";
+export const host = "matrix.redlink.de";
 export const baseUrl = `https://${host}`;
 
 /**
